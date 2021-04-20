6d01683b382879a56d07b3c19c19c3a885e5bf1b
Kebur Lulseged <kebur.lulseged@gmail.com>
2021-04-20 12:13:57 -0700

    create login link from stripe to send to frontend

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -1,5 +1,5 @@
 import User from "../models/user";
-
+import queryString from "query-string";
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 export const createConnectAccount = async (req, res) => {
@@ -15,4 +15,19 @@ export const createConnectAccount = async (req, res) => {
     user.save();
   }
 
+  let accountLink = await stripe.accountLinks.create({
+    account: user.stripe_account_id,
+    refresh_url: process.env.STRIPE_REDIRECT_URL,
+    return_url: process.env.STRIPE_REDIRECT_URL,
+    type: "account_onboarding",
+  });
+
+  accountLink = Object.assign(accountLink, {
+    "stripe_user[email]": user.email || undefined,
+  });
+
+  let link = `${accountLink.url}?${queryString.stringify(accountLink)}`
+  console.log(link);
+  res.send(link);
+  console.log("ACCOUNT LINK", accountLink);
 };
